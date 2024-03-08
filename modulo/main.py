c9973f7bde09820b4d1c10d07e2154e771d6c255
DCalel <diegocalel25@gmail.com>
2024-03-08 17:25:36 -0600

    v0.1.0 api lectura de excel

--- modulo/main.py
+++ modulo/main.py
@@ -1,8 +1,31 @@
+from numpy import DataSource
 import pandas as pd
 import openpyxl as op
+from flask import Flask, request
 
 ruta_excel = 'MOCK_DATA.xlsx'
 
 datos_excel = pd.read_excel(ruta_excel)
 
-print(datos_excel)
+nombres_columas = datos_excel.columns
+nombres_esperados = ['id', 'first_name', 'last_name', 'co', 'company', 'product']
+
+print(datos_excel.iloc[0]["id"])
+
+#print(datos_excel.head(2))
+
+
+app = Flask(__name__)
+
+@app.route("/producto", methods=["GET"])
+def listar_productos():
+    if(set(nombres_esperados) == set(nombres_columas)):
+        productos = datos_excel.columns
+
+        return f"Datos:\n\n{datos_excel.head(4)}"
+    else:
+        print('Hubo un error con los nombres')
+        print('Nombres esperados: ', nombres_esperados)
+        print('Nombres en el documentos: ', nombres_columas)
+        return "Invalid Data, try again", 400
+
